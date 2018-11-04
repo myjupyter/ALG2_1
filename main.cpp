d82bbdd2e314ba200f4caef6366b62f3ada51b5d
Kirill Tkachuk <kirilltchkap@gmail.com>
2018-11-04 08:39:30 +0300

    final?

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <cstring>
 using namespace std;
 
 #define SIZE_OF_ALPHABET 131
@@ -45,11 +46,14 @@ struct Double_hashing {
     }
 };
 
+enum Status {NIL = -1, OCCUPED = 0, DELETED = 1};
+
 template<typename T>
 class HashTable {
   private:
-    vector<string> _table;
-    float _coef = 0;
+    vector<std::string> _table;
+    float _coef = 0.;
+    Status * _status;
     size_t _table_size = 8;
     void rehashing();
   public:
@@ -59,39 +63,55 @@ class HashTable {
     HashTable(HashTable &&) = delete;
     HashTable& operator=(const HashTable &) = delete;
     HashTable& operator=(HashTable &&) = delete;
-    ~HashTable() = default;
+    ~HashTable();
     size_t get_size() const;
     float get_coef() const;
 
     bool Add(const std::string& key);
-    bool Has(const std::string& key) const;
+    long long Has(const std::string& key) const;
     bool Remove(const std::string& key);
 };
 
 template<typename T>
 void HashTable<T>::rehashing() {
+  Status * temp = new Status[_table_size];
+  memcpy(temp, _status, sizeof(Status) * _table_size);
   _table_size *= 2;
-  _coef /= 2;
+  delete [] _status;
+  _status = new Status[_table_size];
+  memset(_status, Status::NIL,  sizeof(Status) * _table_size);
+  _coef = 0.;
   std::vector<std::string> temp_copy(_table);
-  _table.resize(_table_size);
-  std::string NIL("");
-  for(auto it = temp_copy.begin(); it != temp_copy.end(); ++it) {
-    if(*it != NIL) {
-      this->Add(*it);
+  _table.resize(_table_size, std::string(""));
+  for(size_t i = 0; i < _table_size / 2; ++i) {
+    if(temp[i] == Status::OCCUPED) {
+      this->Add(temp_copy[i]);
     }
   }
+  delete [] temp;
 }
 
 template<typename T>
-HashTable<T>::HashTable() : _table(_table_size), _coef(0) {}
+HashTable<T>::HashTable()
+  : _table(_table_size), _coef(0.), _status(new Status[_table_size]) {
+  std::string str("");
+  memset(_status, Status::NIL,  sizeof(Status) * _table_size);
+}
 
 template<typename T>
 HashTable<T>::HashTable(size_t size) {
   while(size > _table_size) {
     _table_size *= 2;
   }
+  _status = new Status[_table_size];
+  memset(_status, Status::NIL,  sizeof(Status) * _table_size);
   _table.resize(_table_size);
-  _coef = 0;
+  _coef = 0.;
+}
+
+template<typename T>
+HashTable<T>::~HashTable() {
+  delete [] _status;
 }
 
 template<typename T>
@@ -107,16 +127,15 @@ float HashTable<T>::get_coef() const {
 template <typename T>
 bool HashTable<T>::Add(const std::string& key) {
   T hash_func;
-  std::string NIL("");
-  std::string DEL("::DELETED::");
   unsigned long long hash_value = hash_func(key, _table_size);
-  while(_table[hash_value] != NIL && _table[hash_value] != DEL) {
+  while(_status[hash_value] != Status::NIL && _status[hash_value] != Status::DELETED) {
     if(_table[hash_value] == key) {
       return false;
     }
     hash_value = hash_func(key, _table.size());
   }
   _table[hash_value] = key;
+  _status[hash_value] = Status::OCCUPED;
   _coef += 1. / _table_size;
   if(_coef >= 3./4) {
     this->rehashing();
@@ -125,21 +144,35 @@ bool HashTable<T>::Add(const std::string& key) {
 }
 
 template <typename T>
-bool HashTable<T>::Has(const std::string& key) const {
+long long HashTable<T>::Has(const std::string& key) const {
   T hash_func;
-  std::string NIL("");
   unsigned long long hash_value = hash_func(key, _table_size);
   while(_table[hash_value] != key) {
-    if(_table[hash_value] == NIL) {
-      return false;
+    if(_status[hash_value] == Status::NIL) {
+      return -1;
     }
+    hash_value = hash_func(key, _table_size);
   }
+  if(_status[hash_value] == Status::DELETED) {
+    return -1;
+  }
+  return hash_value;
+}
+
+template <typename T> 
+bool HashTable<T>::Remove(const std::string & key) {
+  long long hash_value = this->Has(key); 
+  if(hash_value == -1) {
+    return false;
+  }
+  _status[hash_value] = Status::DELETED;
+  _coef -= 1. / _table_size;
   return true;
 }
 
 
 int main(int argc, char** argv) {
-  HashTable<Double_hashing<Horner_hash, rHorner_hash>> hash_table;
+  HashTable<Double_hashing<Horner_hash, rHorner_hash>> hash_table(8);
   int n = 0;
   cin >> n;
   char operation;
@@ -151,11 +184,13 @@ int main(int argc, char** argv) {
         cout << hash_table.Add(key) << endl << hash_table.get_coef() << endl;
         break;
       case '?':
-        cout <<hash_table.Has(key) << endl;
+        cout << hash_table.Has(key) << endl;
+        break;
+      case '-':
+        cout << hash_table.Remove(key) << endl << hash_table.get_coef() << endl;
         break;
       default: break;
     }
   }
-
   return 0;
 }
