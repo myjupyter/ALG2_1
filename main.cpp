04cbc5b595407d819fcf6644b9d137232a3df688
Kirill Tkachuk <kirilltchkap@gmail.com>
2018-11-10 08:39:06 +0300

    not final

--- main.cpp
+++ main.cpp
@@ -6,25 +6,27 @@
 #include <cstring>
 using namespace std;
 
-#define SIZE_OF_ALPHABET 131
+#define SIZE_OF_ALPHABET1 131
+#define SIZE_OF_ALPHABET2 127
+#define COEF 0.75
 
 struct Horner_hash {
-  unsigned long long operator()(std::string key, size_t table_size) {
+  unsigned long long operator()(const std::string & key, const size_t & table_size) {
     unsigned long long hash_value = 0l;
     for(auto it = key.begin(); it != key.end(); ++it) {
-      hash_value += (hash_value * SIZE_OF_ALPHABET + (unsigned long long)(*it));
+      hash_value += (hash_value * SIZE_OF_ALPHABET2 + (unsigned long long)(*it)) % table_size;
     }
-    return hash_value % table_size;
+    return hash_value;
   }
 };
 
 struct rHorner_hash {
-  unsigned long long operator()(const std::string& key, size_t table_size) {
+  unsigned long long operator()(const std::string & key, const size_t & table_size) {
     unsigned long long hash_value = 0l;
     for(auto it = key.rbegin(); it != key.rend(); ++it) {
-      hash_value += (hash_value * SIZE_OF_ALPHABET + (unsigned long long)(*it));
+      hash_value += (hash_value * SIZE_OF_ALPHABET1 + (unsigned long long)(*it)) % table_size;
     }
-    return hash_value % table_size;
+    return hash_value;
   }
 };
 
@@ -35,14 +37,30 @@ struct Double_hashing {
     unsigned long long first_hash = 0l;
     unsigned long long second_hash = 0l;
   public:
-    unsigned long long operator()(std::string key, size_t table_size) {
+    unsigned long long operator()(const std::string & key, const size_t & table_size) {
       if(iteration == 0) {
         Hash1 hash_func1;
         Hash2 hash_func2;
         first_hash = hash_func1(key, table_size);
-        second_hash = hash_func2(key, table_size/2);
+        second_hash = hash_func2(key, table_size/2 - 1);
       }
       return ( first_hash + (2 * second_hash + 1) * iteration++ ) % table_size;
+    }
+};
+
+template <typename Hash>
+struct Square_probe {
+  private:
+    long long iteration = -1;
+    unsigned long long hash = 0l;
+  public:
+    unsigned long long operator()(std::string key, size_t table_size) {
+      if(iteration == -1) {
+        Hash hash_func;
+        hash = hash_func(key, table_size);
+      }
+      hash = (hash + 1 + iteration++) % table_size;
+      return hash;
     }
 };
 
@@ -82,7 +100,7 @@ void HashTable<T>::rehashing() {
   memset(_status, Status::NIL,  sizeof(Status) * _table_size);
   _coef = 0.;
   std::vector<std::string> temp_copy(_table);
-  _table.resize(_table_size, std::string(""));
+  _table.resize(_table_size);
   for(size_t i = 0; i < _table_size / 2; ++i) {
     if(temp[i] == Status::OCCUPED) {
       this->Add(temp_copy[i]);
@@ -94,7 +112,6 @@ void HashTable<T>::rehashing() {
 template<typename T>
 HashTable<T>::HashTable()
   : _table(_table_size), _coef(0.), _status(new Status[_table_size]) {
-  std::string str("");
   memset(_status, Status::NIL,  sizeof(Status) * _table_size);
 }
 
@@ -128,66 +145,63 @@ template <typename T>
 bool HashTable<T>::Add(const std::string& key) {
   T hash_func;
   unsigned long long hash_value = hash_func(key, _table_size);
-  while(_status[hash_value] != Status::NIL && _status[hash_value] != Status::DELETED) {
-    if(_table[hash_value] == key) {
+  for(size_t i = 0; i < _table_size; ++i) {
+    if(_table[hash_value] == key && _status[hash_value] == Status::OCCUPED) {
       return false;
     }
-    hash_value = hash_func(key, _table.size());
-  }
-  _table[hash_value] = key;
-  _status[hash_value] = Status::OCCUPED;
-  _coef += 1. / _table_size;
-  if(_coef >= 3./4) {
-    this->rehashing();
-  }
-  return true;
-}
-
-template <typename T>
-long long HashTable<T>::Has(const std::string& key) const {
-  T hash_func;
-  unsigned long long hash_value = hash_func(key, _table_size);
-  while(_table[hash_value] != key) {
-    if(_status[hash_value] == Status::NIL) {
-      return -1;
+    if(_status[hash_value] == Status::NIL || _status[hash_value] == Status::DELETED) {
+      _table[hash_value] = key;
+      _status[hash_value] = Status::OCCUPED;
+      _coef += 1. / _table_size;
+      if(_coef >= COEF) this->rehashing();
+      return true;
     }
     hash_value = hash_func(key, _table_size);
   }
-  if(_status[hash_value] == Status::DELETED) {
-    return -1;
+  return false;
+}
+
+template <typename T>
+long long HashTable<T>::Has(const std::string & key) const {
+  T hash_func;
+  unsigned long long hash_value = hash_func(key, _table_size);
+  for(size_t i = 0; i < _table_size; ++i) {
+    if(_status[hash_value] != Status::NIL) {
+      if( _table[hash_value] == key && _status[hash_value] != Status::DELETED) {
+        return hash_value;
+      }
+    }
+    else return -1;
+    hash_value = hash_func(key, _table_size);
   }
-  return hash_value;
+  return -1;
 }
 
 template <typename T> 
 bool HashTable<T>::Remove(const std::string & key) {
-  long long hash_value = this->Has(key); 
+  long long hash_value = this->Has(key);
   if(hash_value == -1) {
     return false;
   }
   _status[hash_value] = Status::DELETED;
-  _coef -= 1. / _table_size;
   return true;
 }
 
 
 int main(int argc, char** argv) {
-  HashTable<Double_hashing<Horner_hash, rHorner_hash>> hash_table(8);
-  int n = 0;
-  cin >> n;
+  HashTable<Square_probe<Horner_hash>> hash_table(8);
   char operation;
   std::string key;
-  while(n--) {
-    cin >> operation >> key;
+  while(cin >> operation >> key) {
     switch(operation) {
       case '+':
-        cout << hash_table.Add(key) << endl << hash_table.get_coef() << endl;
+        cout << (hash_table.Add(key) ? "OK" : "FAIL") << endl;
         break;
       case '?':
-        cout << hash_table.Has(key) << endl;
+        cout << (hash_table.Has(key) != -1 ? "OK" : "FAIL") << endl;
         break;
       case '-':
-        cout << hash_table.Remove(key) << endl << hash_table.get_coef() << endl;
+        cout << (hash_table.Remove(key) ? "OK" : "FAIL") << endl;
         break;
       default: break;
     }
